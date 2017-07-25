5079d00c271331847a959b4f35b39b7b5cb493bf
Muhammed Thanish <mnmtanish@gmail.com>
2017-07-25 13:15:13 +0530

    Include type definitions

--- .npm_scripts/build.js
+++ .npm_scripts/build.js
@@ -2,6 +2,7 @@ const shelljs = require('shelljs')
 
 const srcFiles = [
     ['node_modules/snapsvg/dist/snap.svg.slim.js', 'index.js'],
+    ['node_modules/@types/snapsvg/index.d.ts', 'index.d.ts'],
 ]
 
 srcFiles.forEach(([src, dst]) => shelljs.rm(dst))
